b833e75f8f59f2345dc814af8edc963f17e6550e
khoavh <huykhoa630@gmail.com>
2025-04-22 21:11:48 +0700

    change file build

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,42 +1,21 @@
 #!/bin/bash
+set -e
+set -x
 
 echo "Building Nuxt app, tag $TAG"
 
-# Exit on error
-set -e
-
-# Enable debug
-set -x
-
-# Cat env content from Jenkins Credentials to .env file
-# cat "$ENV_FILE" > ./.env
-
-# Debug: show PATH and yarn location
+# cat "$ENV_FILE" > .env
 #Build
 export PATH="$PATH:/usr/local/bin"
-echo "Current PATH: $PATH"
-which yarn || {
-  echo "❌ 'yarn' not found. Please make sure it's installed in your Jenkins agent or Docker image."
-  exit 1
-}
-
-# Install dependencies
 yarn install --frozen-lockfile
-
-# Build Nuxt app
 yarn build
 
-# Prepare for deployment
 rm -rf .output.zip
 zip -r .output.zip .output
 
-OUT=$?
-set +x
-
 if [ $OUT -eq 0 ]; then
-    echo "✅ Nuxt app build completed successfully."
     exit 0
 else
-    echo "❌ Failure: Nuxt app build failed."
+    echo 'Failure: app build'
     exit 1
 fi
