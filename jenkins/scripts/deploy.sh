250c27a3deb8f2a9cbd6f0fb9f80c943d4d59e41
KhoaVH <78124749+Vo-Huy-Khoa@users.noreply.github.com>
2025-04-26 16:38:36 +0700

    update file build

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,29 @@ echo "Deploy app - tag $TAG"
 
 #Enable debugging mode
 set -x
+
+chmod 400 $SERVER_SSH_KEY_FILE
+
+#Upload
+scp -i $SERVER_SSH_KEY_FILE build.zip  $SERVER_USERNAME@$SERVER_URL://home/$SERVER_USERNAME/cicd-jenkins/build.zip
+scp -i $SERVER_SSH_KEY_FILE package.json $SERVER_USERNAME@$SERVER_URL://home/$SERVER_USERNAME/cicd-jenkins/package.json
+scp -i $SERVER_SSH_KEY_FILE yarn.lock $SERVER_USERNAME@$SERVER_URL://home/$SERVER_USERNAME/cicd-jenkins/yarn.lock
+scp -i $SERVER_SSH_KEY_FILE $ENV_FILE $SERVER_USERNAME@$SERVER_URL://home/$SERVER_USERNAME/cicd-jenkins/.env
+
+#Deploy
+ssh -i $SERVER_SSH_KEY_FILE $SERVER_USERNAME@$SERVER_URL '
+  cd cicd-jenkins
+  export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.11.0/bin
+  npm install
+  pm2 stop cicd-jenkins
+  rm -R build
+  unzip build.zip
+  rm -R build.zip
+  pm2 restart cicd-jenkins --update-env --log-date-format "YYYY-MM-DD HH:mm:ss.SSS"
+'
+
 OUT=$?
+
 #Disable debugging mode
 set +x
 
